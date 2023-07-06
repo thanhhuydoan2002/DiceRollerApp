6736e5dad7663860678586fcd66213d8f38e6f51
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-07-06 21:07:09 +0700

    add Roll button

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.dicerollerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener{
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
+
 
 fun main(){
     val myFirstDice = Dice(6)
