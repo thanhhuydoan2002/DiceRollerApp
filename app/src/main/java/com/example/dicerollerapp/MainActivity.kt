9bd87666bd4c700d1f8eb7ac9b2c168065ed3a82
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-07-06 21:15:56 +0700

    add private function rollDice

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -12,23 +12,18 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener{
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+            rollDice()
         }
+    }
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
 
-
-fun main(){
-    val myFirstDice = Dice(6)
-    val diceRoll = myFirstDice.roll()
-    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
-
-    val mySecondDice = Dice(20)
-    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
-}
-
-class Dice(val numSides: Int){
+class Dice(private val numSides: Int){
     fun roll(): Int{
         return  (1..numSides).random()
     }
