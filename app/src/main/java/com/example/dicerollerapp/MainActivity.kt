3240da1f2cd2d162a2554b1c004dec5e1b1384fe
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-07-07 21:18:39 +0700

    optimize code

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -23,14 +23,15 @@ class MainActivity : AppCompatActivity() {
 
         //Update the result on the screen
         val diceImage: ImageView = findViewById(R.id.imageView)
-        when(diceRoll){
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        val drawableR = when(diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableR)
 
         val luckyNumber = 6
         if (diceRoll == luckyNumber){
