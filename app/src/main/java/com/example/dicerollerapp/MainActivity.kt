2e67a1cda57f095c00d81f3059ed29f6bc2aedcd
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-07-08 18:47:51 +0700

    add one more dice and calculate the result

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -20,10 +20,11 @@ class MainActivity : AppCompatActivity() {
         // Create new dice with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceRoll2 = dice.roll()
 
         //Update the result on the screen
         val diceImage: ImageView = findViewById(R.id.imageView)
-        val drawableR = when(diceRoll){
+        val drawableR1 = when(diceRoll){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,15 +32,34 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableR)
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableR1)
+
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        val drawableR2 = when(diceRoll2){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage2.setImageResource(drawableR2)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
+
+        val total = diceRoll + diceRoll2
 
         val luckyNumber = 6
-        if (diceRoll == luckyNumber){
+        if (total == luckyNumber){
             val toast = Toast.makeText(this, "You win!", Toast.LENGTH_SHORT)
             toast.show()
         }
         else{
-            val toast = Toast.makeText(this, "You rolled a $diceRoll. Try again!", Toast.LENGTH_SHORT)
+            val toast = Toast.makeText(this, "You rolled a $total. Try again!", Toast.LENGTH_SHORT)
             toast.show()
             return
         }
